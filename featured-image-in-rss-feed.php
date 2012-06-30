edb5d1ebcd87a70aefc95e544ee33c5b3c6e0ab3
dnesscarkey <dnesscarkey@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-30 10:49:11 +0000

    git-svn-id: https://plugins.svn.wordpress.org/featured-image-in-rss-feed/trunk@565735 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- featured-image-in-rss-feed.php
+++ featured-image-in-rss-feed.php
@@ -4,25 +4,11 @@ Plugin Name: Featured Image In Rss Feed
 Plugin URI: http://dineshkarki.com.np/plugins/featured-image-in-rss-feed
 Description: Add Feature Image to your RSS Feed.
 Author: Dinesh Karki
-Version: 0.1
+Version: 0.2
 Author URI: http://www.dineshkarki.com.np
 */
 
-/*  Copyright 2012  Dinesh Karki  (email : dnesskarki@gmail.com)
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License, version 2, as 
-    published by the Free Software Foundation.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+/*  Copyright 2012  Dinesh Karki  (email : dnesskarki@gmail.com) */
 
 function featuredtoRSS($content) {
 	$fir_rss_image_size 			= get_option('fir_rss_image_size');
@@ -33,8 +19,10 @@ function featuredtoRSS($content) {
 	return $content;
 }
 
-add_filter('the_excerpt_rss', 'featuredtoRSS');
-add_filter('the_content_feed', 'featuredtoRSS');
-
+$fir_license_key_status		= get_option('fir_license_key_status');
+if (($fir_license_key_status != 'trial_expired') && ($fir_license_key_status != 'expired')){
+	add_filter('the_excerpt_rss', 'featuredtoRSS');
+	add_filter('the_content_feed', 'featuredtoRSS');
+}
 include('plugin_interface.php');
 ?>
