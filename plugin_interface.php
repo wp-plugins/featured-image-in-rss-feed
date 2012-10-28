dcebf3468bf895f8c8c1e8dd1bba71231f3770bb
dnesscarkey <dnesscarkey@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-28 14:28:05 +0000

    Donate Link
    git-svn-id: https://plugins.svn.wordpress.org/featured-image-in-rss-feed/tags/0.3@618256 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- plugin_interface.php
+++ plugin_interface.php
@@ -18,6 +18,21 @@ function fir_settings_page() {
 		$fir_rss_image_size	= get_option('fir_rss_image_size');
 	}	
 ?>
+
+<div class="support_education wrap" style="width:250px; line-height:2.0; position:fixed; right:0;">
+<h3>Educate a Child</h3>
+We are sponsoring education for poor children. Donate and help us raise fund for them. For more details click <a href="http://dineshkarki.com.np/educate-child" target="_blank">here</a>
+<br /><br />
+<div align="center">
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="GNJJ22PDAAX48">
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+</div>
+</div>
+
 <div class="wrap">
 <h2>Featured Image In RSS Feed</h2>
 
